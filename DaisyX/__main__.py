1168763bea00490ca640d1cc09ff7ab8cd572b6f
okreend <77939839+okreend@users.noreply.github.com>
2021-03-07 19:30:31 +0530

    Update __main__.py
    Changed alive

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -221,7 +221,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "👩‍💻Hey! I'm alive.\n⚙️All systems online and functioning normally!!\n<b>⏱ Alive time:</b> <code>{}</code>\n✨ Thanks for adding me! ✨".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
