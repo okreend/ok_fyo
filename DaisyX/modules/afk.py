4bbdb3de524ae06cbca5a77687a36fe36ab5cc7d
okreend <77939839+okreend@users.noreply.github.com>
2021-03-08 16:16:35 +0530

    Update afk.py

--- DaisyX/modules/afk.py
+++ DaisyX/modules/afk.py
@@ -65,6 +65,8 @@ def no_longer_afk(update: Update, context: CallbackContext):
                 "{} is finally here!",
                 "Welcome back! {}",
                 "Where is {}?\nIn the chat!",
+                "Our Hero {} is in the chat!",
+                "{} buy a teddy bear for me!",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
