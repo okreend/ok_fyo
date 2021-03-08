a73ba494fa5f66820f14353123c72e961e3a20e4
okreend <77939839+okreend@users.noreply.github.com>
2021-03-08 15:40:56 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -297,7 +297,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "*⚊❮❮❮❮ ｢  Help  for  {}  module 」❯❯❯❯⚊*\n".format(
+                "*👩‍💻 ｢  Help  for  {}  module 」*\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
